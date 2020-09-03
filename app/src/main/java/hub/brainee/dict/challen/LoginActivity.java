8fb9951b86b5bebc55902ec0c99aad9460395275
Kimkims1 <kimanthikevin76@gmail.com>
2020-09-04 01:52:05 +0300

    init-:

--- app/src/main/java/hub/brainee/dict/challen/LoginActivity.java
+++ app/src/main/java/hub/brainee/dict/challen/LoginActivity.java
@@ -3,12 +3,34 @@ package hub.brainee.dict.challen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnFacebook, btnGoogle, btnLogin;
+    private EditText emailLogin, passwordLogin;
+    private TextView forgotLoginPassword, signUpTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // TODO: 04/09/2020 Initialize Views
+        initViews();
+
+
+    }
+
+    private void initViews() {
+        btnFacebook = findViewById(R.id.btnFacebook);
+        btnGoogle = findViewById(R.id.btnGoogle);
+        btnLogin = findViewById(R.id.btnLogin);
+        emailLogin = findViewById(R.id.emailLogin);
+        passwordLogin = findViewById(R.id.passwordLogin);
+        forgotLoginPassword = findViewById(R.id.forgotLoginPassword);
+        signUpTxt = findViewById(R.id.signUpTxt);
     }
 }
